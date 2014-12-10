bd7d65f3dd66ed3a11568ae0a5da82d4cfa89b00
huyongliang <13919162581@139.com>
2014-12-10 13:49:47 +0800

    签名工具类的封装

---
+++ WeiXinSDK/src/org/lut/hyl/weixin/utils/SignatureUtil.java
@@ -0,0 +1,70 @@
+package org.lut.hyl.weixin.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import org.lut.hyl.weixin.GlobalProperties;
+
+/**
+ * 签名工具类<br>
+ * <a href='http://blog.csdn.net/lyq8479/article/details/8944988'>参见柳峰的博客</a>
+ * 
+ * @author LiuFeng
+ *
+ */
+public class SignatureUtil {
+	// 与接口配置信息中的Token要一致
+	private static String token = GlobalProperties.TOKEN;
+
+	/**
+	 * 验证签名
+	 * 
+	 * @param signature
+	 * @param timestamp
+	 * @param nonce
+	 * @return
+	 */
+	public static boolean checkSignature(String signature, String timestamp,
+			String nonce) {
+		String[] arr = new String[] { token, timestamp, nonce };
+		Arrays.sort(arr);
+		StringBuilder content = new StringBuilder();
+		for (int i = 0; i < arr.length; i++) {
+			content.append(arr[i]);
+		}
+		MessageDigest md = null;
+		String tmpStr = null;
+
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+			byte[] digest = md.digest(content.toString().getBytes());
+			tmpStr = byteToStr(digest);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		content = null;
+		return tmpStr != null ? tmpStr.equals(signature.toUpperCase()) : false;
+	}
+
+	private static String byteToStr(byte[] byteArray) {
+		String strDigest = "";
+		for (int i = 0; i < byteArray.length; i++) {
+			strDigest += byteToHexStr(byteArray[i]);
+		}
+		return strDigest;
+	}
+
+	private static String byteToHexStr(byte mByte) {
+		char[] Digit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
+				'B', 'C', 'D', 'E', 'F' };
+		char[] tempArr = new char[2];
+		tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
+		tempArr[1] = Digit[mByte & 0X0F];
+
+		String s = new String(tempArr);
+		return s;
+	}
+
+}
